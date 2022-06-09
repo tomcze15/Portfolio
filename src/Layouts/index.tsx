bc187a9d0879e0f091a1b68e4bbc20d47a6c818d
Tomasz Czerwinski <tomasz.czerwinski.1997@gmail.com>
2022-06-09 21:13:15 +0200

    Set max width for layout.

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -4,11 +4,13 @@ import styled from 'styled-components';
 
 const LayoutWrapper = styled.main`
   height: 100%;
-  width: 100%;
+  max-width: 1400px;
 `;
 
 const Layout: FunctionComponent<ILayoutProps> = ({ children }): JSX.Element => (
-  <LayoutWrapper>{children}</LayoutWrapper>
+  <>
+    <LayoutWrapper>{children}</LayoutWrapper>
+  </>
 );
 
 export default Layout;
--- src/view/App.tsx
+++ src/view/App.tsx
@@ -4,15 +4,18 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import { GlobalStyle, Routes } from 'components';
 import { DarkTheme } from 'themes';
+import Layout from 'Layouts';
 
 const App: FunctionComponent = (): JSX.Element => {
   return (
     <StrictMode>
       <GlobalStyle {...DarkTheme} />
       <ThemeProvider theme={DarkTheme}>
-        <Router>
-          <Routes />
-        </Router>
+        <Layout>
+          <Router>
+            <Routes />
+          </Router>
+        </Layout>
       </ThemeProvider>
     </StrictMode>
   );

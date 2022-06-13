80ebd0e301373a6285ddcca4693520381b480904
Tomasz Czerwinski <tomasz.czerwinski.1997@gmail.com>
2022-06-13 22:53:32 +0200

    Done

--- src/components/Pages/Contact/index.tsx
+++ src/components/Pages/Contact/index.tsx
@@ -1,5 +1,5 @@
 import { Box } from 'components';
-import { CONTACTS } from 'constants/items';
+import { CONTACTS, MEDIA } from 'constants/items';
 import { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
@@ -13,6 +13,24 @@ const MainWrapper = styled.div`
   gap: 100px;
   max-width: 1400px;
   padding-bottom: 750px;
+
+  ${MEDIA.XL} {
+    font-size: 36px;
+    padding-bottom: 550px;
+    gap: 70px;
+  }
+
+  ${MEDIA.M} {
+    font-size: 23px;
+    padding-bottom: 500px;
+    gap: 50px;
+  }
+
+  ${MEDIA.S} {
+    font-size: 18px;
+    padding-bottom: 500px;
+    gap: 50px;
+  }
 `;
 
 const ContactContainer = styled.div`
@@ -29,6 +47,16 @@ const ContactContainer = styled.div`
     transition: 0.5s;
     height: 80px;
     width: 80px;
+
+    ${MEDIA.XL} {
+      height: 50px;
+      width: 50px;
+    }
+
+    ${MEDIA.M} {
+      height: 30px;
+      width: 30px;
+    }
   }
 
   &:hover {
@@ -39,6 +67,15 @@ const ContactContainer = styled.div`
       height: 100px;
       width: 100px;
     }
+  }
+
+  ${MEDIA.XL} {
+    flex-direction: column;
+  }
+
+  ${MEDIA.L} {
+    flex-direction: column;
+    gap: 20px;
   }
 `;
 

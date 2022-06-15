e4f73443e37d7203501b449938abed9894cb1f7a
Tomasz Czerwinski <tomasz.czerwinski.1997@gmail.com>
2022-06-15 23:43:15 +0200

    Deployed website

--- src/components/Pages/Home/index.tsx
+++ src/components/Pages/Home/index.tsx
@@ -141,7 +141,7 @@ const Behind = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
+  height: 100vh;
   display: flex;
   justify-content: center;
   position: absolute;
--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -5,6 +5,7 @@ import { ROUTES_HREF } from 'constants/items';
 
 const Routes: FunctionComponent = (): JSX.Element => (
   <MainRoutes>
+    <Route element={<Home />} path={'/portfolio'} />
     <Route element={<Home />} path={ROUTES_HREF.HOME} />
     <Route element={<Experience />} path={ROUTES_HREF.EXPERIENCE} />
     <Route element={<Accomplishment />} path={ROUTES_HREF.ACCOMPLISHMENT} />
